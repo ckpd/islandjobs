574a4c3ecb02aeb03444f2c9122afbc8a3b1712c
ckpd <charlie.deriggs@gmail.com>
2016-08-18 05:13:44 -0400

    database update

--- webapp/settings.py
+++ webapp/settings.py
@@ -122,9 +122,3 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
