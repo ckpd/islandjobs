d023bfdfb91b6905e21ec2046a439e6d691f77f8
ckpd <charlie.deriggs@gmail.com>
2016-08-18 05:18:25 -0400

    database update

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -14,3 +14,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webapp.settings")
 
 application = get_wsgi_application()
+
+try:
+    from dj_static import Cling
+    
+    application = Cling(get_wsgi_application())
+except:
+    pass
